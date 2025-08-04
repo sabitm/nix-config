8936984b001c30d1820f44e0ef31ef21b28c3f3a
sabitm <sabitmaulanaa@gmail.com>
2025-08-04 20:03:57 +0700

    Add android-tools and adbfs

--- modules/system.nix
+++ modules/system.nix
@@ -28,6 +28,8 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
+    adbfs-rootless
+    android-tools
     fd
     gcc
     git
