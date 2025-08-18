7df64bc23d01d9af1056e39d31534bf3e9f13bb2
sabitm <sabitmaulanaa@gmail.com>
2025-08-18 21:43:19 +0700

    Add gnome keybindings and some config

--- modules/home/gnome.nix
+++ modules/home/gnome.nix
@@ -20,12 +20,35 @@ in
     "org/gnome/desktop/search-providers" = {
       disable-external = true;
     };
-    # Extensions
-    "org/gnome/shell".enabled-extensions = map (ext: ext.extensionUuid) extensions;
+    "org/gnome/shell" = {
+      # Extensions
+      enabled-extensions = map (ext: ext.extensionUuid) extensions;
+      # Make alt-tab switch on current workspace only
+      "app-switcher/current-workspace-only" = true;
+    };
     # Keybinds
     "org/gnome/desktop/wm/keybindings" = {
       # Disable alt-space
       activate-window-menu = [];
+      # Windows related
+      maximize = ["<Control><Super>k"];
+      # Navigation
+      move-to-workspace-right = ["<Shift><Super>k"];
+      move-to-workspace-left = ["<Shift><Super>j"];
+      switch-to-workspace-left = ["<Super>j"];
+      switch-to-workspace-right = ["<Super>k"];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      # Disable meta-l keybinding for locking the screen
+      screensaver = [];
+      # Media keys
+      volume-down = ["<Super>F11"];
+      volume-up = ["<Super>F12"];
+      volume-mute = ["<Super>F10"];
+    };
+    # Preferences
+    "org/gnome/desktop/wm/preferences" = {
+      resize-with-right-button = true;
     };
   };
 
