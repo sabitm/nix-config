f34d06c575b907aa17a49b3d904f465cbc30a9e9
sabitm <sabitmaulanaa@gmail.com>
2025-10-04 16:52:06 +0700

    Add gnome mime settings

--- modules/system/gnome.nix
+++ modules/system/gnome.nix
@@ -9,4 +9,19 @@
   environment.gnome.excludePackages = with pkgs; [
     gnome-console
   ];
+
+  # Mime settings
+  xdg.mime = {
+    enable = true;
+    defaultApplications = {
+      "image/jpeg" = "org.gnome.Loupe.desktop";
+      "image/png" = "org.gnome.Loupe.desktop";
+      "image/gif" = "org.gnome.Loupe.desktop";
+      "image/bmp" = "org.gnome.Loupe.desktop";
+      "image/webp" = "org.gnome.Loupe.desktop";
+      "image/svg+xml" = "org.gnome.Loupe.desktop";
+      "image/heif" = "org.gnome.Loupe.desktop";
+      "image/tiff" = "org.gnome.Loupe.desktop";
+    };
+  };
 }
