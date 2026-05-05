49eb57fde54763439842fcae8bde2294d3f71962
sabitm <sabitmaulanaa@gmail.com>
2026-05-05 21:58:22 +0700

    Add fish binding to clear terminal screen

--- data/fish/userconf.d/bind.fish
+++ data/fish/userconf.d/bind.fish
@@ -6,3 +6,6 @@ bind alt-enter accept-autosuggestion execute
 
 # bind alt-comma to copy previous word and paste it
 bind alt-comma backward-kill-bigword yank yank
+
+# bind ctrl-; to clear terminal screen
+bind ctrl-\; 'echo -ne "\e[H\e[2J\e[3J"; commandline -f repaint'
