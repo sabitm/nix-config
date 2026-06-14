118aba326b97a20a31e7e8d271efecc94595a319
sabitm <sabitmaulanaa@gmail.com>
2026-06-14 20:53:44 +0700

    Raise GTT limit for vbox-min

--- modules/hosts/vbox.nix
+++ modules/hosts/vbox.nix
@@ -13,9 +13,14 @@
     hostModule = ../../hosts/vbox;
     desktop = false;
     extraModules = [
+      config.flake.modules.nixos.ssh
       # base enables zram at normal priority; mkForce to opt this host out.
       ({ lib, ... }: { zramSwap.enable = lib.mkForce false; })
-      config.flake.modules.nixos.ssh
+      # amdgpu caps GTT (GPU-usable system RAM) at half of RAM = 8 GiB by default.
+      # This raise it to 13 GiB (3256738 * 4 KiB pages), leaving 3 GiB for the host.
+      ({ ... }: {
+        boot.kernelParams = [ "ttm.pages_limit=3256738" "ttm.page_pool_size=3256738" ];
+      })
     ];
   };
 }
